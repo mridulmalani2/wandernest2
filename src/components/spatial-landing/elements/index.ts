57afa8fca5e2acaa7d1f85758c78534b2c5f4a60
Mridul Anil Malani <mridul.malani@alumni.ashoka.edu.in>
2025-12-22 12:24:23 +0100

    Revert "Implement spatial 3D landing page with scroll-driven camera"

--- src/components/spatial-landing/elements/index.ts
+++
@@ -1,3 +0,0 @@
-export { SpatialText, HeadlineText, SubheadlineText, BodyText } from './SpatialText'
-export { SpatialButton, BlueCTAButton, PurpleCTAButton } from './SpatialButton'
-export { FeatureCard3D } from './FeatureCard3D'
--- src/components/spatial-landing/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { useCameraScroll, DEFAULT_SECTIONS, DEFAULT_CAMERA_CONFIG } from './useCameraScroll'
